7c99da44707fe031eef626c15637441cb6007afc
Yang XuePing <xueping.yang@gmail.com>
2014-05-21 18:03:15 +0800

    add ssdb client

--- config.js
+++ config.js
@@ -2,5 +2,9 @@ module.exports = {
   resources: {
     prefix : '/resources/',
     root   : '/Users/ququzone/Develop/temp/wms/files/resources'
+  },
+  ssdb: {
+    host : 'localhost',
+    port : '9999'
   }
 }
---
+++ store.js
@@ -0,0 +1,5 @@
+var ssdb = require('SSDB');
+
+exports.set = function() {
+  //
+}
